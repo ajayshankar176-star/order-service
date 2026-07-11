b67a995353dad896472d94d0389295bcb5237cb2
ajayshankar176-star <ajayshankar176@gmail.com>
2026-07-11 09:47:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db=SQLAlchemy()
 
-class User(db.Model):
-    _tablename_ = "users"
+class Order(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
+    _tablename_="orders"
+
+    id=db.Column(db.Integer,primary_key=True)
+
+    user_id=db.Column(db.Integer)
+
+    product=db.Column(db.String(100))
+
+    quantity=db.Column(db.Integer)
+
+    price=db.Column(db.Float)
